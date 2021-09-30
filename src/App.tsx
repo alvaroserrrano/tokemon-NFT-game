5f589149b1bed4769aa6ba3d5126ffc9776fc0d8
Alvaro Serrano <alvaro.serrano@wmich.edu>
2021-09-30 19:03:51 -0400

    setting background image using react styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import backgroundImageColor from './assets/background/backgroundImageColor.png';
 import './App.css';
-import backgroundImage from './assets/background/backgroundImageColor.png';
 
 function App(): JSX.Element {
   return (
-    <>
-      <div className='w-full absolute top-0 left-0 h-full bg-cover bg-fixed'>
-        <img src={backgroundImage} />
-      </div>
-    </>
+    <div
+      style={{
+        width: '100%',
+        height: '100%',
+        objectFit: 'cover',
+        position: 'absolute',
+        backgroundImage: `url(${backgroundImageColor})`,
+        top: 0,
+        left: 0,
+      }}
+    >
+      <p>hello</p>
+    </div>
   );
 }
 
